c788740caa1a4f9fbc77e9ec943e06a2a5a08937
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-08-18 10:49:37 +0300

    float devision

--- kube/read-model/queries/20-ReadModel.sql
+++ kube/read-model/queries/20-ReadModel.sql
@@ -7,7 +7,7 @@ CREATE STREAM PublicSchemes_WithUsers WITH (
     event.Version as SchemaVersion,
     event.Payload->ColorScheme as ColorScheme,
     event.Payload->ColorScheme->colorSchemeName as Name,
-    EXTRACTJSONFIELD('[\"dark\", \"light\"]', '$[' + CAST(ROUND(CAST(event.Payload->ColorScheme->backgroundLightnessLimit as DOUBLE) / 100) as VARCHAR) + ']') as Side,
+    EXTRACTJSONFIELD('[\"dark\", \"light\"]', '$[' + CAST(ROUND(CAST(event.Payload->ColorScheme->backgroundLightnessLimit as DOUBLE) / 100.0) as VARCHAR) + ']') as Side,
     user.UserId as PublisherId,
     user.DisplayName as PublisherName,
     user.NormalizedRoleName <> 'OWNER' as PublisherCommunity
